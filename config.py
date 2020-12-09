93d3369724207e5f1b3c116413ef58042cad8d8e
nanxiaodi <13934779844@qq.com>
2020-12-09 15:18:06 +0800

    修改配置

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=gr74HiRR7XHhGByIQXsen4; __jsluid_h=73fe21c6b52214eba76361b90ff48dbe; device_id=213070643315939556423335886273fa6ed332b8a9c572cda1889e5d88; __jsluid_s=cbc952809e0e9c89625f5746e8698cd1; _ga=GA1.2.83030652.1593955644; smzdm_user_source=6EE9B059A8445AEB9D6B185A60DA0D73; wt3_sid=%3B999768690672041; r_sort_type=score; userId=user:1899482046|1899482046; sess=MzVkYzN8MTYxMDg3MDA4OHwxODk5NDgyMDQ2fDUwNjZmZGQzNzEzYjE2Njg5YzM1ZTlkZTQ4NmNlNDg2; user=user%3A1899482046%7C1899482046; smzdm_id=1899482046; smzdm_ec=06; smzdm_ea=200; _gid=GA1.2.1275334236.1607331175; homepage_sug=c; _zdmA.vid=*; footer_floating_layer=0; ad_date=7; wt3_eid=%3B999768690672041%7C2160127177700963674%232160733131100415317; _zdmA.uid=ZDMA.3VnOBa7al.1607332284.2419200; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1605681638,1607332285; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1607332285; ad_json_feed=%7B%22J_feed_ad1%22%3A%7B%22number%22%3A0%2C%22surplus%22%3A2%7D%7D; bannerCounter=%5B%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%5D; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2217375204aa5828-01b0c54e2f971b-15356650-1296000-17375204aa69d%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fgithub.com%2Fstark666%2Fsmzdm_bot%22%2C%22%24latest_utm_source%22%3A%22chrome%22%2C%22%24latest_utm_medium%22%3A%22Push%22%2C%22%24latest_utm_campaign%22%3A%22Tixing%22%7D%2C%22%24device_id%22%3A%2217375204aa5828-01b0c54e2f971b-15356650-1296000-17375204aa69d%22%7D; zdm_qd=%7B%22utm_source%22%3A%22chrome%22%2C%22utm_medium%22%3A%22Push%22%2C%22utm_campaign%22%3A%22Tixing%22%2C%22referrer%22%3A%22https%3A%2F%2Fgithub.com%2Fstark666%2Fsmzdm_bot%22%7D; _gat_UA-27058866-1=1; amvid=2410319f0b60ac3261e85cac64c89aaa; __gads=ID=530673fa7689338a-228dab5812c500d2:T=1607332291:RT=1607332291:S=ALNI_Mbkq7J-uVmdrNkv7f8FRrSSventRg; _zdmA.time=1607332294242.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
 调试用 SERVERCHAN_SECRETKEY
 """
-SERVERCHAN_SECRETKEY = ''
+SERVERCHAN_SECRETKEY = 'SCU76983T317e7ea54d18e5d6feedd5f17fd7a7175e1836da03f80'
